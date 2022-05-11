340808e899a32a1367d488bf98f1d565934a07b1
Victor Donizete <victor.donizete87@gmail.com>
2022-05-11 16:16:15 -0300

    login-page

---
+++ .history/src/pages/LoginPage_20220511161342.tsx
@@ -0,0 +1,43 @@
+import '../styles/login-page.scss'
+
+import googleIconIMG from '../assets/login-page/googleicon.svg' 
+
+export function LoginPage() {
+  return (
+    <div id="auth">
+      <main>
+        <div className="main-content">
+          <strong>WELCOME BACK</strong>
+          <p>Welcome back! Please enter your detalis</p>
+          <div className="form">
+            <p>Email</p>
+            <input type='text' name='Email' placeholder='Email' />
+            <p>Password</p>
+            <div>
+              <input type="text" name='Password' placeholder='Password'
+              checked />
+
+
+            </div>
+          </div>
+          <div className="checkbox">
+            <input type="checkbox" id='remember-me' name="remember-me" />
+            <label htmlFor="Password">Remember me</label>
+
+            <a href="#">Forgot password</a>
+          </div>
+          <button>
+            <i><img src={googleIconIMG} alt="" /></i>
+            Sign in with Google
+          </button>
+          <p>Don't have an account? <a href="#">Sign up for free!</a></p>
+
+        </div>
+      </main>
+      <aside>
+        
+
+      </aside>
+    </div>
+  )
+}
